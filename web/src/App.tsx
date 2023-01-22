82c03dcdc3c6dfcf312c6a8653a9de4c6d3da037
vagnermaltauro <vagnermaltauro@gmail.com>
2023-01-21 21:23:38 -0300

    First commit - Habit

---
+++ web/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles/global.css";
+
+import Habit from "./components/Habit";
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+
+      <Habit completed={10} />
+
+      <Habit completed={20} />
+
+      <Habit completed={30} />
+    </div>
+  );
+}
+
+export default App;
