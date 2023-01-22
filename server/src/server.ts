129504b59aedd3348a6e02ceab52a25cf259818c
vagnermaltauro <vagnermaltauro@gmail.com>
2023-01-22 15:47:57 -0300

    Feat - config routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,11 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { appRoutes } from "./lib/routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({
   port: 3333,
