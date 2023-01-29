1c2bd719d1c4ad5c73db4368e892924b58c97949
vagnermaltauro <vagnermaltauro@gmail.com>
2023-01-29 15:12:10 -0300

    Feat: finalizing route creation

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -1,6 +1,6 @@
+import request from 'http';
 import dayjs from 'dayjs';
 import { FastifyInstance } from 'fastify';
-import { request } from 'http';
 import { z } from 'zod';
 import { prisma } from './prisma';
 
@@ -68,5 +68,80 @@ export async function appRoutes(app: FastifyInstance) {
       prossibleHabits,
       completedHabits,
     };
+  });
+
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitsParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitsParams.parse(request.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day!.id,
+          habit_id: id,
+        }
+      }
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day!.id,
+          habit_id: id,
+        }
+      });
+    }
+  });
+
+  app.get('/summary', async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT
+        D.id,
+        D.date,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+           ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+    return summary;
   });
 }
