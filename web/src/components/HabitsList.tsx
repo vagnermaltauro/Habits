d92bd4fc975603fa09fbbff3dcfdd4205a127d1f
vagnermaltauro <vagnermaltauro@gmail.com>
2023-02-05 11:31:59 -0300

    Feat: include habitsList

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Check } from 'phosphor-react';
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { api } from '../lib/axios';
+import dayjs from 'dayjs';
 
 interface HabitsListProps {
   date: Date;
@@ -18,6 +19,7 @@ interface HabitsInfo {
 
 export function HabitsList({ date }: HabitsListProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
+  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
 
   useEffect(() => {
     api.get('day', {
@@ -36,6 +38,7 @@ export function HabitsList({ date }: HabitsListProps) {
           <Checkbox.Root
             key={habit.id}
             checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
             className="flex items-center gap-3 group">
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
               <Checkbox.Indicator>
